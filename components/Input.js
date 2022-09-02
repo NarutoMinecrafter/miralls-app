ee3bb2d306d869707a1d447547c7087e0cc659e2
NarutoMinecrafter <www.iwanmayor.com@gmail.com>
2022-09-03 02:32:40 +0300

    return old inputs

--- components/Input.js
+++ components/Input.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Colors } from "../constants";
+import { View, TextInput, Pressable, Text } from "react-native";
+import { Sizes, Colors } from "../constants";
 import { useTogglePasswordVisibility } from "./hooks/useTogglePasswordVisibility";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import {
-  IconButton,
-  TextInput,
-  ThemeProvider,
-  useTheme,
-} from "@react-native-material/core";
 
 export default function Input({
   name,
   placeholder,
+  placeholderTextColor,
   passwordVisiblityButton = false,
   secureTextEntry,
   textContentType,
@@ -33,43 +29,39 @@ export default function Input({
   const { passwordVisibility, rightIcon, handlePasswordVisibility } =
     useTogglePasswordVisibility();
   const [textInputValue, setTextInputValue] = React.useState(value);
-  const theme = useTheme();
-
-  const newTheme = {
-    ...theme,
-    palette: {
-      ...theme.palette,
-      surface: {
-        main: theme.palette.surface.main,
-        on:
-          validationError || validationErrorText
-            ? Colors.Input.ValidationError.Color
-            : theme.palette.surface.on,
-      },
-    },
-  };
-
-  const styles = {
-    backgroundColor: "black",
-    color:
-      validationError || validationErrorText
-        ? Colors.Input.ValidationError.Color
-        : Colors.White,
-  };
 
   if (value != textInputValue) setTextInputValue(value);
 
   return (
-    <ThemeProvider theme={newTheme}>
+    <View style={[wrapperStyle]}>
+      {label ? (
+        <Text
+          style={[
+            style.Label,
+            validationError || validationErrorText
+              ? style.LabelValidationError
+              : null,
+          ]}
+        >
+          {label}
+        </Text>
+      ) : null}
+
       <TextInput
-        style={[wrapperStyle, styles]}
-        inputContainerStyle={styles}
-        inputStyle={[textInputStyle, styles]}
+        style={[
+          style.TextInput,
+          textInputStyle,
+          validationError || validationErrorText
+            ? style.TextInputValidationError
+            : null,
+        ]}
         name={name}
-        label={placeholder || label}
-        variant="outlined"
-        color={validationError || validationErrorText ? "error" : "primary"}
-        helperText={validationErrorText}
+        placeholder={placeholder}
+        placeholderTextColor={
+          validationError || validationErrorText
+            ? Colors.Input.ValidationError.PlaceholderTextColor
+            : placeholderTextColor
+        }
         secureTextEntry={
           passwordVisiblityButton ? passwordVisibility : secureTextEntry
         }
@@ -86,24 +78,63 @@ export default function Input({
         onEndEditing={onEndEditing}
         value={textInputValue}
         multiline={multiline}
-        trailing={
-          passwordVisiblityButton
-            ? (props) => (
-                <IconButton
-                  onPress={handlePasswordVisibility}
-                  icon={(props) => (
-                    <MaterialCommunityIcons
-                      name={rightIcon}
-                      size={22}
-                      color={Colors.White}
-                    />
-                  )}
-                  {...props}
-                />
-              )
-            : undefined
-        }
       ></TextInput>
-    </ThemeProvider>
+      {passwordVisiblityButton ? (
+        <View style={pressableWrapperStyle}>
+          <Pressable onPress={handlePasswordVisibility}>
+            <MaterialCommunityIcons
+              name={rightIcon}
+              size={22}
+              color={Colors.White}
+            />
+          </Pressable>
+        </View>
+      ) : (
+        <></>
+      )}
+      {validationErrorText && (
+        <Text style={style.ErrorText}>{validationErrorText}</Text>
+      )}
+    </View>
   );
 }
+
+const style = {
+  TextInput: {
+    backgroundColor: Colors.Input.BackgroundColor,
+    color: Colors.Input.Color,
+    paddingVertical: Sizes.Input.PaddingVertical,
+    paddingHorizontal: Sizes.Input.PaddingHorizontal,
+    fontSize: Sizes.Input.FontSize,
+    borderRadius: Sizes.Input.BorderRadius,
+    borderWidth: Sizes.Input.BorderWidth,
+    borderColor: Colors.Input.BorderColor,
+  },
+  TextInputValidationError: {
+    borderColor: Colors.Input.ValidationError.BorderColor,
+    color: Colors.Input.ValidationError.Color,
+  },
+  Label: {
+    color: Colors.InputLabel.Color,
+    marginBottom: 8,
+    marginLeft: 4,
+    fontFamily: "SF-Regular",
+    fontSize: 16,
+  },
+  LabelValidationError: {
+    color: Colors.InputLabel.ValidationError.Color,
+  },
+  ErrorText: {
+    color: Colors.InputLabel.ValidationError.Color,
+  },
+};
+
+const pressableWrapperStyle = {
+  display: "flex",
+  alignItems: "center",
+  position: "absolute",
+  right: 15,
+  // backgroundColor: 'red',
+  top: Sizes.Input.MarginTop + 6,
+  height: "100%",
+};
